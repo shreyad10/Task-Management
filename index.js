e65b6c42e43cbc173a39b96c220c30affc280245
ShreyaDWappgo <shreya.dubey@wappgo.in>
2024-07-04 13:56:32 +0530

    update

--- index.js
+++ index.js
@@ -13,28 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+
+// Enable CORS for all routes
 app.use(cors());
 
 // Connecting to the database
 require("./app/config/connection.js");
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type,Authorization"
-  );
-
-  next();
-});
 
 app.use(route);
 app.use("/", (req, res)=> {
