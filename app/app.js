dea2f30b6c1f97c79abf594cbe0c41dc40669bc9
ShreyaDWappgo <shreya.dubey@wappgo.in>
2024-07-04 15:53:40 +0530

    update

--- app/app.js
+++ app/app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const projectRoutes = require('./routes/projectRoutes');
-const taskRoutes = require('./routes/taskRoutes');
-const { errorHandler } = require('./middlewares/errorHandler');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const authRoutes = require('./routes/authRoutes');
+// const projectRoutes = require('./routes/projectRoutes');
+// const taskRoutes = require('./routes/taskRoutes');
+// const { errorHandler } = require('./middlewares/errorHandler');
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/projects', projectRoutes);
-app.use('/api/tasks', taskRoutes);
+// app.use(bodyParser.json());
+// app.use('/api/auth', authRoutes);
+// app.use('/api/projects', projectRoutes);
+// app.use('/api/tasks', taskRoutes);
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
-module.exports = app;
+// module.exports = app;
