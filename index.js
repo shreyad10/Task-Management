caa47e5fde98c722d9084336a5b7626537833d25
ShreyaDWappgo <shreya.dubey@wappgo.in>
2024-07-04 16:14:12 +0530

    update

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ dotenv.config();
 
 // create express app
 const app = express();
-// Enable CORS for all routes
-app.use(cors());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,14 +14,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
+// Enable CORS for all routes
+app.use(cors({
+  origin: '*', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Connecting to the database
 require("./app/config/connection.js");
 
 app.use(route);
-// app.use("/", (req, res)=> {
-//   res.send("Hello world")
-// })
+app.use("/", (req, res)=> {
+  res.send("Hello world")
+})
 
 // listen for requests
 app.listen(process.env.PORT, () => {
