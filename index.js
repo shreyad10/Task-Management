7c6657f2e33b2ddb011a3f276899706c81779598
ShreyaDWappgo <shreya.dubey@wappgo.in>
2024-07-04 18:57:36 +0530

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 
 // Connecting to the database
 require("./app/config/connection.js");
